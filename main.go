21a8cb3ae58dda1176dbcfae89f600f213f744dc
Dmitriy Morkulev <morkulev.dim@yandex.ru>
2024-11-23 20:07:36 +0300

    собственный ServeMux (роутер)

--- main.go
+++ main.go
@@ -14,8 +14,33 @@ func hello(w http.ResponseWriter, req *http.Request) {
 } 
 
 func main() {
-	http.HandleFunc("/hello", hello)
+
+	/*
+	ServeMux:
+		Принимает входящие HTTP-запросы
+		Сопоставляет URL-пути с зарегистрированными обработчиками (handlers)
+		Направляет запросы в соответствующие обработчики
+	*/
+
+
+	/*
+	DefaultServeMux является глобальной переменной в пакете http, это означает что:
+		Любая часть программы может регистрировать в нем обработчики через http.HandleFunc()
+		Все зарегистрированные маршруты хранятся в одном месте
+		При масштабировании приложения разные пакеты могут случайно перезаписать маршруты друг друга
+	*/
+
+	// Поэтому нужно использовать собственный serveMux для каждого из модулей
 	port:=8081
+	
+	router := http.NewServeMux() // создаем новый ServeMux
+	router.HandleFunc("/hello", hello)
+	server := http.Server{ // конфигурируем сервер через структуру
+		Addr:    fmt.Sprintf(":%d", port),
+		Handler: router,
+	}
+
+
 	fmt.Printf("Server is listening on port %d ...", port)
-	http.ListenAndServe(fmt.Sprintf(":%d", port), nil) // начинаем обработку TCP-соединений на порту 8080 и передаем в него дефолтный обработчик (ServeMux - принимает запросы и передает их в соответствующие обработчики)
+	server.ListenAndServe()
 }
