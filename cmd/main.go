f74b96f36f8daad525035d5fa6d4aa4b94835da7
dmitrii.morkulev <dmitrii.morkulev@tages.ru>
2024-12-04 18:08:23 +0300

    context.WithTimeout

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"link-shortner-api/configs"
 	"link-shortner-api/internal/auth"
@@ -9,9 +10,30 @@ import (
 	"link-shortner-api/pkg/db"
 	"link-shortner-api/pkg/middleware"
 	"net/http"
+	"time"
 )
 
 func main() {
+	ctx := context.Background()                                       // создаем базовый контекст
+	ctxWithTimeout, cancel := context.WithTimeout(ctx, 2*time.Second) // из него создаем контекст с таймаутом
+	defer cancel()                                                    // если выполнили быстрее таймаута, то отменим контекст
+
+	done := make(chan struct{})
+
+	go func() {
+		time.Sleep(3 * time.Second)
+		close(done)
+	}()
+
+	select {
+	case <-done:
+		fmt.Println("Done task")
+	case <-ctxWithTimeout.Done():
+		fmt.Println("Timeout")
+	}
+}
+
+func main2() {
 	port := 8081
 	config := configs.LoadConfig()
 
