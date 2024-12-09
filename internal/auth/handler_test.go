f9288f5bc1051a6033d3ef9204622a77d95efc4d
dmitrii.morkulev <dmitrii.morkulev@tages.ru>
2024-12-09 14:44:54 +0300

    интеграционные тесты: мокаем http-запрос и запрос в БД

--- internal/auth/handler_test.go
+++ internal/auth/handler_test.go
@@ -1,10 +1,14 @@
 package auth_test
 
 import (
+	"bytes"
+	"encoding/json"
 	"link-shortner-api/configs"
 	"link-shortner-api/internal/auth"
 	"link-shortner-api/internal/user"
 	"link-shortner-api/pkg/db"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 
 	"github.com/DATA-DOG/go-sqlmock"
@@ -12,19 +16,16 @@ import (
 	"gorm.io/gorm"
 )
 
-func TestLoginSuccess(t *testing.T) {
-	// go get github.com/DATA-DOG/go-sqlmock
+func bootstrap() (*auth.AuthHandler, sqlmock.Sqlmock, error) {
 	database, mock, err := sqlmock.New() // создали мок БД
 	if err != nil {
-		t.Fatalf("Failed init mock db: %s: ", err)
-		return
+		return nil, nil, err
 	}
 	gormDb, err := gorm.Open(postgres.New(postgres.Config{
 		Conn: database,
 	})) // получили полноценный инстанс gorm, но при этом не поднимаем реальную БД (мокнули БД)
 	if err != nil {
-		t.Fatalf("Failed init gorm: %s: ", err)
-		return
+		return nil, nil, err
 	}
 	defer database.Close()
 	userRepo := user.NewUserRepository(&db.Db{ // создали репозиторий с мокнутой БД
@@ -39,5 +40,32 @@ func TestLoginSuccess(t *testing.T) {
 		},
 		AuthService: auth.NewAuthService(userRepo),
 	}
+	return &handler, mock, nil
+}
 
+func TestLoginSuccess(t *testing.T) {
+	handler, mock, err := bootstrap()
+	if err != nil {
+		t.Fatal(err)
+	}
+	rows := sqlmock.NewRows([]string{"email", "password"}).
+		AddRow("test@example.com", "$2a$10$GQS/HGBh4TyMG1.69MulZeL2B9uhOZWGfUow0PCD3.RkOigzYY0Dq")
+	// доавили виртуальную строку в моковую БД (пароль зашифрован)
+
+	mock.ExpectQuery("SELECT email, password FROM \"users\" WHERE email = ?").
+		WithArgs("test@example.com").
+		WillReturnRows(rows) // указываем при каком запросе (в данном случае выборка по email) какие данные должны быть возвращены
+	data, _ := json.Marshal(&auth.LoginRequest{
+		Email:    "test@example.com",
+		Password: "password",
+	})
+
+	reader := bytes.NewReader(data)
+	wr := httptest.NewRecorder()                                       // создаем структуру записи ответа (респонса)
+	req := httptest.NewRequest(http.MethodPost, "/auth/login", reader) // создаем структуру запроса
+	handler.Login()(wr, req)
+
+	if wr.Code != http.StatusOK {
+		t.Fatalf("Expected status code %d, got %d", http.StatusOK, wr.Code)
+	}
 }
